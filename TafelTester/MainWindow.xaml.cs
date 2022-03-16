aa8ce548b320de016a48ec589e08e7581f41b006
LexB901 <lexbrinkman2002@gmail.com>
2022-03-16 20:44:18 +0100

    Tafeltester finisho

--- TafelTester/MainWindow.xaml.cs
+++ TafelTester/MainWindow.xaml.cs
@@ -20,11 +20,19 @@ namespace TafelTester
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+    public static class GLOBALS
+    {
+        public static int score { get; set; }
+        
+    }
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
+
         }
         private new void PreviewTextInput(object sender, TextCompositionEventArgs e)
         {
@@ -32,46 +40,168 @@ namespace TafelTester
             e.Handled = regex.IsMatch(e.Text);
         }
 
+        public bool IsDefault { get; set; }
+
+        private void ButtonBack_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonBack.Visibility = Visibility.Collapsed;
+            ButtonEasy.Visibility = Visibility.Visible;
+            ButtonNormal.Visibility = Visibility.Visible;
+            ButtonHard.Visibility = Visibility.Visible;
+            QuestionEasyGrid.Visibility = Visibility.Collapsed;
+            QuestionNormalGrid.Visibility = Visibility.Collapsed;
+            QuestionHardGrid.Visibility = Visibility.Collapsed;
+            AnsEasyCorrect.Visibility = Visibility.Collapsed;
+            AnsEasyWrong.Visibility = Visibility.Collapsed;
+            AnsNormalCorrect.Visibility = Visibility.Collapsed;
+            AnsNormalWrong.Visibility = Visibility.Collapsed;
+            AnsHardCorrect.Visibility = Visibility.Collapsed;
+            AnsHardWrong.Visibility = Visibility.Collapsed;
+            EasyScore.Text = "Score: 0";
+            NormalScore.Text = "Score: 0";
+            HardScore.Text = "Score: 0";
+            GLOBALS.score = 0;
+        }
+
         private void EasyQuestionGrid(object sender, RoutedEventArgs e)
         {
             ButtonEasy.Visibility = Visibility.Collapsed;
             ButtonNormal.Visibility = Visibility.Collapsed;
             ButtonHard.Visibility = Visibility.Collapsed;
+            ButtonBack.Visibility = Visibility.Visible;
             QuestionEasyGrid.Visibility = Visibility.Visible;
+            Random rnd = new Random();
+            EasyNum1.Text = rnd.Next(0, 11).ToString();
+            EasyNum2.Text = rnd.Next(0, 11).ToString();
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+        private void NormalQuestionGrid(object sender, RoutedEventArgs e)
         {
             ButtonEasy.Visibility = Visibility.Collapsed;
             ButtonNormal.Visibility = Visibility.Collapsed;
             ButtonHard.Visibility = Visibility.Collapsed;
+            ButtonBack.Visibility = Visibility.Visible;
+            QuestionNormalGrid.Visibility = Visibility.Visible;
+            Random rnd = new Random();
+            NormalNum1.Text = rnd.Next(10, 21).ToString();
+            NormalNum2.Text = rnd.Next(0, 11).ToString();
         }
 
-        private void Button_Click_2(object sender, RoutedEventArgs e)
+        private void HardQuestionGrid(object sender, RoutedEventArgs e)
         {
             ButtonEasy.Visibility = Visibility.Collapsed;
             ButtonNormal.Visibility = Visibility.Collapsed;
             ButtonHard.Visibility = Visibility.Collapsed;
+            ButtonBack.Visibility = Visibility.Visible;
+            QuestionHardGrid.Visibility = Visibility.Visible;
+            Random rnd = new Random();
+            HardNum1.Text = rnd.Next(10, 21).ToString();
+            HardNum2.Text = rnd.Next(0, 11).ToString();
         }
 
         private void EasyQuestion(object sender, RoutedEventArgs e)
         {
-            ButtonEasyGrid.Visibility = Visibility.Collapsed;
-            AnsCorrect.Visibility = Visibility.Collapsed;
-            AnsWrong.Visibility = Visibility.Collapsed;
-            QuestionEasy.Visibility = Visibility.Visible;
-            Random rnd = new Random();
-            int RandomNum1 = rnd.Next(1, 11);
-            int RandomNum2 = rnd.Next(1, 11);
-            int sum = RandomNum1 + RandomNum2;
-            EasyNum1.Content = RandomNum1;
-            EasyNum2.Content = RandomNum2;
-            int SumAns = Convert.ToInt32(EasyInput);
-            if (SumAns == sum)
+            AnsEasyCorrect.Visibility = Visibility.Collapsed;
+            AnsEasyWrong.Visibility = Visibility.Collapsed;
+            QuestionEasyGrid.Visibility = Visibility.Visible;
+            if (String.IsNullOrEmpty(EasyInput.Text))
             {
-                AnsCorrect.Visibility = Visibility.Visible;
-
+                AnsEasyEmpty.Content = "Textbox is empty!";
+                AnsEasyEmpty.Visibility = Visibility.Visible;
             }
+            else if (int.Parse(EasyNum1.Text) + int.Parse(EasyNum2.Text) == int.Parse(EasyInput.Text))
+            {
+                AnsEasyEmpty.Visibility = Visibility.Collapsed;
+                AnsEasyCorrect.Visibility = Visibility.Visible;
+                GLOBALS.score += 1;
+                EasyScore.Text = "Score: " + GLOBALS.score;
+                EasyInput.Text = "";
+                Random rnd = new Random();
+                EasyNum1.Text = rnd.Next(0, 11).ToString();
+                EasyNum2.Text = rnd.Next(0, 11).ToString();
+            }
+            else
+            {
+                AnsEasyEmpty.Visibility = Visibility.Collapsed;
+                AnsEasyWrong.Visibility = Visibility.Visible;
+                GLOBALS.score += 0;
+                EasyScore.Text = "Score: " + GLOBALS.score;
+                EasyInput.Text = "";
+                Random rnd = new Random();
+                EasyNum1.Text = rnd.Next(0, 11).ToString();
+                EasyNum2.Text = rnd.Next(0, 11).ToString();
+            }
+        }
+        private void NormalQuestion(object sender, RoutedEventArgs e)
+        {
+            AnsNormalCorrect.Visibility = Visibility.Collapsed;
+            AnsNormalWrong.Visibility = Visibility.Collapsed;
+            QuestionNormalGrid.Visibility = Visibility.Visible;
+
+
+            if (String.IsNullOrEmpty(NormalInput.Text))
+            {
+                AnsNormalEmpty.Content = "Textbox is empty!";
+                AnsNormalEmpty.Visibility = Visibility.Visible;
+            }
+            else if (int.Parse(NormalNum1.Text) - int.Parse(NormalNum2.Text) == int.Parse(NormalInput.Text))
+            {
+                AnsNormalEmpty.Visibility = Visibility.Collapsed;
+                AnsNormalCorrect.Visibility = Visibility.Visible;
+                GLOBALS.score += 1;
+                NormalScore.Text = "Score: " + GLOBALS.score;
+                NormalInput.Text = "";
+                Random rnd = new Random();
+                NormalNum1.Text = rnd.Next(10, 21).ToString();
+                NormalNum2.Text = rnd.Next(0, 11).ToString();
+            }
+            else
+            {
+                AnsNormalEmpty.Visibility = Visibility.Collapsed;
+                AnsNormalWrong.Visibility = Visibility.Visible;
+                GLOBALS.score += 0;
+                NormalScore.Text = "Score: " + GLOBALS.score;
+                NormalInput.Text = "";
+                Random rnd = new Random();
+                NormalNum1.Text = rnd.Next(10, 21).ToString();
+                NormalNum2.Text = rnd.Next(0, 11).ToString();
+            }
+
+        }
+        private void HardQuestion(object sender, RoutedEventArgs e)
+        {
+            AnsHardCorrect.Visibility = Visibility.Collapsed;
+            AnsHardWrong.Visibility = Visibility.Collapsed;
+            QuestionHardGrid.Visibility = Visibility.Visible;
+
+            if (String.IsNullOrEmpty(HardInput.Text))
+            {
+                AnsHardEmpty.Content = "Textbox is empty!";
+                AnsHardEmpty.Visibility = Visibility.Visible;
+            }
+            else if (int.Parse(HardNum1.Text) * int.Parse(HardNum2.Text) == int.Parse(HardInput.Text))
+            {
+                AnsHardEmpty.Visibility = Visibility.Collapsed;
+                AnsHardCorrect.Visibility = Visibility.Visible;
+                GLOBALS.score += 1;
+                HardScore.Text = "Score: " + GLOBALS.score;
+                HardInput.Text = "";
+                Random rnd = new Random();
+                HardNum1.Text = rnd.Next(10, 21).ToString();
+                HardNum2.Text = rnd.Next(0, 11).ToString();
+            }
+            else
+            {
+                AnsHardEmpty.Visibility = Visibility.Collapsed;
+                AnsHardWrong.Visibility = Visibility.Visible;
+                GLOBALS.score += 0;
+                HardScore.Text = "Score: " + GLOBALS.score;
+                HardInput.Text = "";
+                Random rnd = new Random();
+                HardNum1.Text = rnd.Next(10, 21).ToString();
+                HardNum2.Text = rnd.Next(0, 11).ToString();
+            }
+
         }
     }
 }
